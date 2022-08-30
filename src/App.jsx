d634faae76ee338371fc6d4a2cf2c6b0da8d2040
Caroline Maia Abreu da Silva <carolmas99@gmail.com>
2022-08-30 17:50:22 -0300

    project structure added

---
+++ src/App.jsx
@@ -0,0 +1,30 @@
+import { FiSearch } from 'react-icons/fi'
+
+function App() {
+  return (
+    <div className="container">
+      <h1 className="title">Buscador CEP</h1>
+
+      <div className="containerInput">
+        <input type="text" 
+        placeholder="Digite seu cep..."
+        />
+
+        <button className="buttonSearch">
+          <FiSearch size={25} color="#FFF"/>
+        </button>
+      </div>
+
+      <main className='main'>
+        <h2>Cep: 00000000</h2>
+        <span>Complemento: Complemento</span>
+        <span>Partenon</span>
+        <span>Campo Grande -RS</span>
+      </main>
+      
+
+    </div>
+  )
+}
+
+export default App
